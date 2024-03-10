bd4fd753fad601f0f7daf5a6aeae0367cfd99e67
Nykolas Laurentino de Lima <nykolas.lima@gmail.com>
2024-03-10 18:18:22 -0300

    Adicionando atualizacao de variante de produto
    Permitindo a atualizacao de uma variante existente e tambem
    ajustando a exibicao de preco de produtos com variantes para
    exibir o texto de "A partir de R$ X".

--- src/restaurant_hub/application/controller/catalog_controller.py
+++ src/restaurant_hub/application/controller/catalog_controller.py
@@ -161,6 +161,19 @@ def load_add_product_variant_to_product(product_id: str):
     return render_template('catalog/product_variant.html', product=product, section=section)
 
 
+@main.route('/products/<product_id>/product-variants/<product_variant_id>', methods=['GET'])
+@auth
+def load_update_product_variant_in_product(product_id: str, product_variant_id: str):
+    product_id = uuid.UUID(product_id)
+    product = ProductRepository().load(product_id)
+    section = SectionRepository().load(product.section_id)
+    product_variant_id = uuid.UUID(product_variant_id)
+    product_variant = next((variant for variant in product.variants if variant.id == product_variant_id), None)
+
+    return render_template('catalog/product_variant.html', product=product, section=section,
+                           **_to_product_variant_form(product_variant))
+
+
 @main.route('/products/<product_id>/product-variants', methods=['POST'])
 @auth
 def add_product_variant(product_id: str):
@@ -177,6 +190,37 @@ def add_product_variant(product_id: str):
         price=price,
         image_key=None)
     result = ProductVariantService().add(product_variant, product, image.read() if image else None)
+
+    if result.is_left():
+        return render_template('catalog/partials/add_product_variant_form.html', messages=result.left(),
+                               product=product,
+                               **_to_product_variant_form(product_variant))
+    else:
+        response = make_response()
+        response.headers['HX-Redirect'] = url_for('main.load_update_product_in_section', section_id=product.section_id,
+                                                  product_id=product.id)
+        return response
+
+
+@main.route('/products/<product_id>/product-variants', methods=['PATCH'])
+@auth
+def update_product_variant(product_id: str):
+    product_id = uuid.UUID(product_id)
+    product = ProductRepository().load(product_id)
+    id = uuid.UUID(string_form_value('id'))
+    product_variant = next((variant for variant in product.variants if variant.id == id), None)
+    if not product_variant:
+        response = make_response()
+        response.status_code = 404
+        return response
+    name = string_form_value('name')
+    description = string_form_value('description')
+    price = int(decimal_form_value('price') * 100) if decimal_form_value('price') else None
+    image = request.files['image'] if 'image' in request.files else None
+    product_variant.name = name
+    product_variant.description = description
+    product_variant.price = price
+    result = ProductVariantService().update(product_variant, product, image.read() if image else None)
 
     if result.is_left():
         return render_template('catalog/partials/add_product_variant_form.html', messages=result.left(),
